cecd2a559af3f0cbe69e82ddc40b626e74b4dab2
JayChavan-23 <jaychavan2223@gmail.com>
2023-02-24 17:50:39 +0530

    pagination, filter and search done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,8 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+
 
 
 //Create Product -- Admin
@@ -17,10 +19,19 @@ exports.createProduct = catchAsyncErrors(async(req,res,next)=>{
 //Get All Products
 exports.getAllProducts = catchAsyncErrors(async(req,res) => {
 
-    const products = await Product.find();
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
-        products
+        products,
+        productCount
     })
 
 });
